d3971ce6d2ddb8b288dce454430bfda65d44401d
Kirill Denisenko <kirill@stereowind.com>
2018-10-29 18:31:21 +0900

    02.3 Implicit Intents done

---
+++ ImplicitIntents/app/src/main/java/com/stereowind/training/implicitintents/MainActivity.java
@@ -0,0 +1,66 @@
+package com.stereowind.training.implicitintents;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v4.app.ShareCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText mWebsiteEditText;
+    private EditText mLocationEditText;
+    private EditText mShareTextEditText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mWebsiteEditText = findViewById(R.id.website_edittext);
+        mLocationEditText = findViewById(R.id.location_edittext);
+        mShareTextEditText = findViewById(R.id.share_edittext);
+    }
+
+    public void openWebsite(View view) {
+        // Get url text
+        String url = mWebsiteEditText.getText().toString();
+        // Pass url to new Uri object and create an intent with url data
+        Uri webpage = Uri.parse(url);
+        Intent intent = new Intent(Intent.ACTION_VIEW, webpage);
+        // Check that at least one other app can resolve the intent
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        } else {
+            Log.d("ImplicitIntents", "Can't handle this!");
+        }
+    }
+
+    public void openLocation(View view) {
+        // Get location text
+        String loc = mLocationEditText.getText().toString();
+        // Pass it as Geo-URL into a new Uri object and create an intent
+        Uri addressUri = Uri.parse("geo:0,0?q=" + loc);
+        Intent intent = new Intent(Intent.ACTION_VIEW, addressUri);
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        } else {
+            Log.d("ImplicitIntents", "Can't handle this intent!");
+        }
+    }
+
+    public void shareText(View view) {
+        // Get text data
+        String txt = mShareTextEditText.getText().toString();
+        // Define text type
+        String mimeType = "text/plain";
+        ShareCompat.IntentBuilder
+                .from(this)
+                .setType(mimeType)
+                .setChooserTitle(R.string.share_with_txt)
+                .setText(txt)
+                .startChooser();
+    }
+}

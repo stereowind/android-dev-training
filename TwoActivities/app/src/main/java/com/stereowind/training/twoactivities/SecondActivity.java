76024608d7954ad2824f311469337086558face8
Kirill Denisenko <kirill@stereowind.com>
2018-10-29 15:58:02 +0900

    02.2 done, added onSaveInstanceState() and log callbacks

--- TwoActivities/app/src/main/java/com/stereowind/training/twoactivities/SecondActivity.java
+++ TwoActivities/app/src/main/java/com/stereowind/training/twoactivities/SecondActivity.java
@@ -3,17 +3,57 @@ package com.stereowind.training.twoactivities;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 
 public class SecondActivity extends AppCompatActivity {
 
+    private static final String LOG_TAG = SecondActivity.class.getSimpleName();
     public static final String EXTRA_REPLY = "com.stereowind.training.twoactivities.extra.REPLY";
     private EditText mReply;
 
     @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(LOG_TAG, "onStart");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(LOG_TAG, "onStop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(LOG_TAG, "onDestroy");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(LOG_TAG, "onPause");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(LOG_TAG, "onResume");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d(LOG_TAG, "onRestart");
+    }
+
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Log.d(LOG_TAG, "-------");
+        Log.d(LOG_TAG, "onCreate");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
         mReply = findViewById(R.id.editText_second);
@@ -29,5 +69,7 @@ public class SecondActivity extends AppCompatActivity {
         replyIntent.putExtra(EXTRA_REPLY, reply);
         setResult(RESULT_OK,replyIntent);
         finish();
+        Log.d(LOG_TAG, "End SecondActivity");
     }
+
 }
